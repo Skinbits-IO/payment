100d2c2e2ff9ea0e00ccc6124d425eb28df332a9
Artyomcha <artyomswim@gmail.com>
2024-12-17 21:13:58 +0100

    Check

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,8 @@ import { TonConnectButton } from "@tonconnect/ui-react";
 import { useMainContract } from "./hooks/useMainContract";
 import { useTonConnect } from "./hooks/useTonConnect";
 import { fromNano } from "ton-core";
-
-import WebApp from '@twa-dev/sdk'
-import Panel from './components/panel.tsx'
-
-//WebApp.showAlert('Hey there!');
+import WebApp from '@twa-dev/sdk';
+import Panel from './components/panel.tsx';
 
 function App() {
   const {
@@ -19,109 +16,88 @@ function App() {
     sendWithdrawalRequest
   } = useMainContract();
 
-  const {connected } = useTonConnect();
-  const handlePayWithTON = (skinName: string) => {
-    console.log(`Paying with TON for skin: ${skinName}`);
+  const { connected } = useTonConnect();
+
+  const handlePayWithTON = (skinName: string, price: number) => {
+    console.log(`Paying with TON for ${skinName} at ${price} TON.`);
   };
 
-  const handlePayWithStars = (skinName: string) => {
-    console.log(`Paying with Stars for skin: ${skinName}`);
+  const handlePayWithStars = (skinName: string, price: number) => {
+    console.log(`Paying with Stars for ${skinName} at ${price} TON equivalent.`);
   };
-
-//const showAlert = () => {
- // WebApp.showAlert("Hey there!");
-//};
 
   return (
     <div>
-      <div>
-      <div className="Container">
-          <Panel
-            skinName="CS2 Skin 1"
-            description="A cool skin for CS2 with excellent graphics and design."
-            price={1.5} // Price in TON
-            onPayWithTON={handlePayWithTON}
-            onPayWithStars={handlePayWithStars}
-          />
-          <Panel
-            skinName="CS2 Skin 2"
-            description="A cool skin for CS2 with excellent graphics and design."
-            price={2.0}
-            onPayWithTON={handlePayWithTON}
-            onPayWithStars={handlePayWithStars}
-          />
-          <Panel
-            skinName="CS2 Skin 3"
-            description="A cool skin for CS2 with excellent graphics and design."
-            price={3.25}
-            onPayWithTON={handlePayWithTON}
-            onPayWithStars={handlePayWithStars}
-          />
+      {!connected ? (
+        // If not connected, center the TonConnectButton
+        <div className="CenterContainer">
+          <TonConnectButton />
         </div>
-      </div>
+      ) : (
+        // Once connected, move the TonConnectButton to the top-right and show the panels
+        <>
+          <div className="TonConnectContainer">
+            <TonConnectButton />
+          </div>
 
-      <div>
-        <TonConnectButton />
-      </div>
-      <div>
-        <div className='Card'>
-          <b>{WebApp.platform}</b>
-          <b>Our contract Address</b>
-          <div className='Hint'>{contract_address?.slice(0, 30) + "..."}</div>
-          <b>Our contract Balance</b>
-          {contract_balance &&
-            <div className='Hint'>{fromNano(contract_balance)}</div>
+          <div className="Container">
+            <Panel
+              skinName="CS2 Skin 1"
+              description="A cool skin for CS2 with excellent graphics and design."
+              price={1.5} // Price in TON
+              onPayWithTON={handlePayWithTON}
+              onPayWithStars={handlePayWithStars}
+            />
+            <Panel
+              skinName="CS2 Skin 2"
+              description="A cool skin for CS2 with excellent graphics and design."
+              price={2.0}
+              onPayWithTON={handlePayWithTON}
+              onPayWithStars={handlePayWithStars}
+            />
+            <Panel
+              skinName="CS2 Skin 3"
+              description="A cool skin for CS2 with excellent graphics and design."
+              price={3.25}
+              onPayWithTON={handlePayWithTON}
+              onPayWithStars={handlePayWithStars}
+            />
+          </div>
 
-          }
-          
-        </div>
+          <div>
+            <div className='Card'>
+              <b>{WebApp.platform}</b>
+              <b>Our contract Address</b>
+              <div className='Hint'>{contract_address?.slice(0, 30) + "..."}</div>
+              <b>Our contract Balance</b>
+              {contract_balance && (
+                <div className='Hint'>{fromNano(contract_balance)}</div>
+              )}
+            </div>
 
-        <div className='Card'>
-          <b>Counter Value</b>
-          <div>{counter_value ?? "Loading..."}</div>
-        </div>
+            <div className='Card'>
+              <b>Counter Value</b>
+              <div>{counter_value ?? "Loading..."}</div>
+            </div>
 
-        {connected && (
-        <a onClick={ () => {
-         //showAlert();
-        }}
-        >
-          Show Alert
-        </a>
+            <br/>
+
+            <a onClick={() => sendIncrement()}>
+              Increment by 5
+            </a>
+            <br/>
+
+            <a onClick={() => sendDeposit()}>
+              Request deposit of 1 TON
+            </a>
+            <br/>
+
+            <a onClick={() => sendWithdrawalRequest()}>
+              Request 0.7 TON withdrawal
+            </a>
+          </div>
+        </>
       )}
-
-      <br/>
-
-      {connected && (
-        <a onClick={ () => {
-          sendIncrement();
-        }}
-        >
-          Increment by 5
-        </a>
-      )}
-      <br/>
-
-       {connected && (
-        <a onClick={ () => {
-          sendDeposit();
-        }}
-        >
-          Request deposit of 1 TON
-        </a>
-      )} 
-
-      <br/>
-
-      {connected && (
-        <a onClick={ () => {
-          sendWithdrawalRequest();
-      }}
-      >
-        Request  0.7 TON withdrawal
-      </a>
-      )} 
-      </div>
     </div>
   );
 }
